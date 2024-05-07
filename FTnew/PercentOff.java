f5d1e09c52d1c09593dc1850b92624a99d36899e
Alfi Muhammad Ghiffari <149681773+nivy2534@users.noreply.github.com>
2024-05-07 16:44:22 +0700

    Update PercentOff.java

--- FTnew/PercentOff.java
+++ FTnew/PercentOff.java
@@ -1,5 +1,12 @@
 package FTnew;
 
 public class PercentOff extends Promotion {
-
+  /*
+  ketika dipanggil, maka akan mereturn atau memberikan fungsi diskon
+  ketika inisialiasi
+  PercentOff (generate ranodm String atau kode sendiri) = new PercentOff();
+  -> (isi) double diskon
+  -> harga setelah diskon akan ada di class order
+  
+  */
 }
