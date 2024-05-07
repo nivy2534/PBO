ce699f9eb92328e839ceee39f8e98da2c65d2798
Alfi Muhammad Ghiffari <149681773+nivy2534@users.noreply.github.com>
2024-05-07 14:07:16 +0700

    Update Order.java

--- FTnew/Order.java
+++ FTnew/Order.java
@@ -6,8 +6,10 @@ package FTnew;
 
 import java.time.LocalDate;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Order {
+    Scanner sc = new Scanner(System.in);
     LocalDate dateOrder;
     int noOrder;
     double subprice;
@@ -18,9 +20,44 @@ public class Order {
     Applicable applicable;
     Car car;
 
-    // public void makeOrder(){
+    public void makeOrder(String destination, String pickupAddress, String phoneNumber, LocalDate date, Car car) {
+        // String destination, pickupAddress, phoneNumber;
+        // LocalDate date;
+        generateNumberOrder();
+        while (true) {
+            System.out.println("Seat tersedia : (0 tersedia, 1 tidak tersedia)");
+            car.availableSeat();
+            System.out.println("Silahkan pilih seat Anda : (no urut terkecil dari paling kiri dan terdepan)");
+            System.out.println("1. seat 1");
+            System.out.println("2. seat 2");
+            System.out.println("3. seat 3");
+            System.out.println("4. seat 4");
+            System.out.println("5. seat 5");
+            System.out.println("6. seat 6");
+            System.out.println("7. seat 7");
+            System.out.println("0. exit");
+            int choice = sc.nextInt();
+            if (choice > 4) {
+                int seatRow = choice - 5;
+                car.seat[2][seatRow] = true;
+            } else if (choice > 1) {
+                int seatRow = choice - 2;
+                car.seat[1][seatRow] = true;
+            } else
+                car.seat[0][0] = true;
 
-    // }
+            if (choice == 0)
+                break;
+        }
+    }
+
+    public Order() {
+
+    }
+
+    public Order(String destination, String pickupAddress, String phoneNumber, LocalDate date, Car car) {
+        makeOrder(destination, pickupAddress, phoneNumber, date, car);
+    }
 
     public void generateNumberOrder() {
         Random rand = new Random();
@@ -34,11 +71,13 @@ public class Order {
     }
 
     public void printDetails(String fullName, String phoneNumber, String destination, String pickupAddress) {
+        System.out.println(orderStatus);
+        System.out.println();
         System.out.println("Nama: " + fullName);
         System.out.println("Nomor HP: " + phoneNumber);
         System.out.println("Nomor pesanan: " + noOrder);
         System.out.println("Alamat penjemputan: " + pickupAddress);
-        System.out.println("Mobil: Avanza putih dengan plat N 1234 O");
+        // System.out.println("Mobil: Avanza putih dengan plat N 1234 O");
     }
 
     public void applyPromo(Promotion x) {
