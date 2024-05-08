985896b16bce0e0cc4a85dad5bfe75507b98560a
WahyuLillah69 <149341353+WahyuLillah69@users.noreply.github.com>
2024-05-09 00:54:44 +0700

    Promotion.java

--- FTnew/Promotion.java
+++ FTnew/Promotion.java
@@ -8,6 +8,19 @@ public abstract class Promotion implements Comparable, Applicable {
     private String promoCode;
     private int minPrice;
 
+    public Promotion(String promoCode, int minPrice) {
+		this.promoCode = promoCode;
+        this.minPrice = minPrice;
+	}
+
+    public String getPromoCode(){
+        return promoCode;
+    }
+
+    public int getMinPrice(){
+        return minPrice;
+    }
+
     public boolean isCostumerEligible(Customer x) {
         if (x instanceof Member) {
             if (x.getAccountAge() > 30) {
