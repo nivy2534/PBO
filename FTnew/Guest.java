c6b18e573a251c81b05e56ee45112a941c449425
Alfi Muhammad Ghiffari <149681773+nivy2534@users.noreply.github.com>
2024-05-09 16:57:19 +0700

    Update Guest.java

--- FTnew/Guest.java
+++ FTnew/Guest.java
@@ -7,14 +7,17 @@ package FTnew;
 import java.time.LocalDate;
 import java.util.Date;
 
+import online_book_store.customer;
+
 public class Guest extends Customer {
     public Guest(String firstName, String lastName, String id) {
         super(firstName, lastName, id);
     }
 
     @Override
-    public void makeOrder(String destination, String pickupAddress, String phoneNumber, Date date, Car car) {
-        super.orderPool[orderIndex] = new Order(destination, pickupAddress, phoneNumber, date, car);
+    public void makeOrder(String destination, String pickupAddress, String phoneNumber, Date date, Car car,
+            Customer customer) {
+        super.orderPool[orderIndex] = new Order(destination, pickupAddress, phoneNumber, date, car, customer);
         orderIndex++;
         // Order order = new Order();
         // order.generateNumberOrder();
